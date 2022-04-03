b57622b985fe9b2f09d52d0abc64465b800f2918
aaigan <102907427+aaigan@users.noreply.github.com>
2022-04-04 01:31:41 +0300

    Update Siralama.java

--- Siralama.java
+++ Siralama.java
@@ -11,28 +11,26 @@ public class Siralama {
         System.out.print("c sayısı:");
         c = inp.nextInt();
 
-        // a > b > c, a > c > b, b > c > a, b > a > c, c > a > b, c > b > a
+        // a < b < c, a < c < b, b < a < c, b < c < a, c < b < a, c < a < b
 
-        if (( a > b ) && ( a > c )){
-            if ( b > c ){
-                System.out.println("a > b > c");
+        if (( a < b ) && ( a < c )){
+            if ( b < c ){
+                System.out.println("a < b < c");
             }else {
-                System.out.println("a > c > b");
+                System.out.println("a < c < b");
             }
-        }else if (( b > a ) && ( b > c )){
-            if ( a > c ){
-                System.out.println("b > a > c");
+        }else if (( b < a ) && ( b < c )){
+            if ( a < c ){
+                System.out.println("b < a < c");
             }else {
-                System.out.println("b > c > a");
+                System.out.println("b < c < a");
             }
         }else {
-            if ( b > a ){
-                System.out.println("c > b > a");
+            if ( b < a ){
+                System.out.println("c < b < a");
             }else {
-                System.out.println("c > a > b");
+                System.out.println("c < a < b");
             }
         }
     }
 }
-
-
