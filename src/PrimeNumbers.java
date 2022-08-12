0edf1473e9df2dd9f3e059cb8df74b1d100c9e1c
Ali Igan <aaigan@hotmail.com>
2022-08-12 16:06:53 +0300

    Update Project

--- src/PrimeNumbers.java
+++ src/PrimeNumbers.java
@@ -1,17 +1,22 @@
 public class PrimeNumbers {
     public static void main(String[] args) {
-        // i li dongu istenilen aralıktaki sayıları yazdırıyor
-        // j 'li dongü i ye gelene kadar olan sayıları yazdırıyor
-           // çünkü sayının çarpanları sayının kendisinden küçük veya sayıya eşit olmak durumunda
-        // while ifadesinin içinde i aralığındaki sayıların asal çarpanlarına ayrılmış hali var
-        for (int i = 1; i <= 20; i++) {
-            for (int j = 1;  j <= i; j++){
-               while (i%j == 0){
-                   System.out.println(j);
-                   break;
-               }
+        // 1'den 100'e kadar olan asal sayıları yazdır.
+
+        int sayac;
+
+        for (int i = 2; i < 100; i++){
+            sayac =0;
+            for (int j = 2; j < i; j++){
+                if (i % j == 0){
+                    sayac++;
+                }
             }
+            if (sayac == 0){
+                System.out.print(i + " ");
             }
 
         }
     }
+}
+
+
