10e57a105c084733471aee901ec99cdd1c2e3548
Ali Igan <aaigan@hotmail.com>
2022-08-13 15:22:59 +0300

    Add

--- src/DiamondWithStars.java
+++ src/DiamondWithStars.java
@@ -5,7 +5,27 @@ public class DiamondWithStars {
         Scanner inp = new Scanner(System.in);
         System.out.print("Enter a number : ");
         int n = inp.nextInt();
-
+        for ( int i = 1; i <= n; i++){
+            for (int j = 1; j <= (n - i); j++){
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= ((2 * i) - 1); k++){
+                System.out.print("*");
+            }
+            // herbir satırdaki işlem tamamlandığında bir alt satıra inmesi için;
+            System.out.println(" ");
+            if (i == n){
+                for (int a = (n - 1); a >= 1; a--){
+                    for (int b = a; b < n; b++){
+                        System.out.print(" ");
+                    }
+                    for (int c = 1; c <= ((2 * a) - 1); c++){
+                        System.out.print("*");
+                    }
+                    System.out.println("");
+                }
+            }
+        }
 
     }
 }
