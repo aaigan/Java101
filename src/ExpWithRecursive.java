f254c093bb069adf5d7a0bbe96bfb3047a19779f
Ahmet Ali İĞAN <102907427+aliigan@users.noreply.github.com>
2022-08-23 15:14:21 +0300

    Updated

--- src/ExpWithRecursive.java
+++ src/ExpWithRecursive.java
@@ -1,13 +1,7 @@
 import java.util.Scanner;
 
 public class ExpWithRecursive {
-    static void exp(int a, int b){
-        int result = 1;
-        for ( int i = 1; i <= b; i++ ){
-            result *= a;
-        }
-        System.out.println(result);
-    }
+    
     static int result =1;
     static int f(int x, int y){
 
